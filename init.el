93bf4c6a497441de0171c78449844cf21efbc9c9
Joshua Suskalo <joshua@suskalo.org>
2019-05-12 11:05:57 -0500

    Tangle and load a skeleton org config

--- init.el
+++ init.el
@@ -28,6 +28,33 @@
       org-src-fontify-natively t
       org-src-tab-acts-natively t)
 
+;; Check if the literate config has been tangled recently
+(let ((aeon-config (expand-file-name "aeon.org" user-emacs-directory))
+      (aeon-source (expand-file-name "aeon.el" user-emacs-directory)))
+  (when (or
+	 (and (file-exists-p aeon-config)
+	      (file-exists-p aeon-source)
+	      (file-newer-than-file-p
+	       aeon-config
+	       aeon-source))
+	 (not (file-exists-p aeon-source)))
+    (org-babel-tangle-file aeon-config)))
+
+;; Check if the config has been compiled recently
+(let ((aeon-source (expand-file-name "aeon.el" user-emacs-directory))
+      (aeon-compiled (expand-file-name "aeon.elc" user-emacs-directory)))
+  (when (or
+	 (and (file-exists-p aeon-source)
+	      (file-exists-p aeon-compiled)
+	      (file-newer-than-file-p
+	       aeon-source
+	       aeon-compiled))
+	 (not (file-exists-p aeon-compiled)))
+    (byte-compile-file aeon-source)))
+
+;; Load the config
+(load-file (expand-file-name "aeon.elc" user-emacs-directory))
+
 ;; Disable the debugger now that the config has loaded
 (setq debug-on-error nil)
 (setq debug-on-quit nil)
