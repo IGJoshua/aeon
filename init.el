0dfc67b57357abb74e4aed10e0a577a7b775eef4
Joshua Suskalo <joshua@suskalo.org>
2019-05-12 11:04:50 -0500

    Add straight.el bootstrap

--- init.el
+++ init.el
@@ -1,3 +1,17 @@
 ;;;; init.el
 ;;; Basic initialization code for the AEON literate emacs config
 ;;; Tangles the aeon.org file in the emacs directory and loads it
+
+;; Bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
