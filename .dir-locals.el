8000437dd8198cb74f9b211576a50823524e699e
Joshua Suskalo <joshua@suskalo.org>
2019-06-19 23:01:38 -0500

    Remove directory-local file which caused annoying prompt on startup

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-((org-mode . ((eval . (add-hook 'after-save-hook
-                                (lambda ()
-                                  (let ((tangled-files (org-babel-tangle (buffer-file-name) nil 'emacs-lisp)))
-                                    (cl-loop for file in tangled-files
-                                             do
-                                             (byte-compile-file file t))))
-                                nil
-                                t)))))
